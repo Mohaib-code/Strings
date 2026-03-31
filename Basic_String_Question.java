fd04659fb39d184600f99b961bd2b1527be777e0
Mohaib Molla <mohaibmolla1@gmail.com>
2026-03-31 10:10:54 +0530

    done

--- Basic_String_Question.java
+++ Basic_String_Question.java
@@ -47,14 +47,60 @@ public class Basic_String_Question {
         }
         System.out.println(largest);
     }
+    public static void string_builder(){
+        StringBuilder sb=new StringBuilder("");
+        for(char ch='A';ch<='Z';ch++){
+            sb.append(ch);
+        }
+        System.out.println(sb);
+        System.out.println(sb.length());
+    }
+    public static String upperCase(String str){
+        StringBuilder sb=new StringBuilder("");
+        char ch=Character.toUpperCase(str.charAt(0));
+        sb.append(ch);
+        for (int i = 1; i < str.length(); i++) {
+            if(str.charAt(i) == ' '&& i<str.length()-1 ){
+                sb.append(str.charAt(i));
+                i++;
+                sb.append(Character.toUpperCase(str.charAt(i)));
+            }
+            else{
+                sb.append(str.charAt(i));
+            }
+        }
+        System.out.println(sb);
+        return sb.toString();
+        
+    }
+    public static String compression(String str){
+     StringBuilder sb=new StringBuilder("");
+     for (int i = 0; i < str.length(); i++) {
+        Integer count=1;
+        while(i<str.length()-1 && str.charAt(i)== str.charAt(i+1) ){
+            count++;
+            i++;
+        }
+        sb.append(str.charAt(i));
+        if(count >1){
+            sb.append(count);
+        }
+     }  
+     return sb.toString(); 
+    }
     public static void main(String args[]){
         String str="WNEENESENNNN";
         String str2="hello World";
-        // System.out.println(shortestPath(str));
-        // System.out.println(palindrome_check(str));
+        System.out.println(shortestPath(str));
+        System.out.println(palindrome_check(str));
         System.out.println(substring(str2, 3, 7));
         String fruits[]={"apple","mango","banana"};
         largest(fruits);
+        string_builder();
+        upperCase("hi, i am tony bhaiya  ");
+        String repeat="aaabbcddd";
+        System.out.println(compression(repeat));
+
 
     }
 }
